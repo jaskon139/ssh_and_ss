bc03a2581d8588bc8b366fe31b0198de497e7b6f
jaskon139 <jaskon139@gmail.com>
2017-11-15 06:10:15 +0800

    sss

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,7 +18,7 @@ echo "root:password" | chpasswd
 #export KCP_MODE=fast 
 #export KCP_MUT=1350 
 #export KCP_NOCOMP=''
-#ss-server -s 0.0.0.0 -p 3721 -k laogao -m aes-256-cfb -t 300 -a nobody --fast-open -d 8.8.8.8 -d 8.8.4.4 -u -f /tmp/ss.pid &
+ss-server -s 0.0.0.0 -p 3721 -k laogao -m aes-256-cfb -t 300 -a nobody --fast-open -d 8.8.8.8 -d 8.8.4.4 -u -f /tmp/ss.pid &
 #              -m $METHOD \
 #              -t $TIMEOUT \
 #              -a $USER \
@@ -27,7 +27,7 @@ echo "root:password" | chpasswd
 #              -d $DNS_ADDR_2 \
 #              $UDP \
 #              -f /tmp/ss.pid &
-#server_linux_amd64 -t "127.0.0.1:$SERVER_PORT" -l ":3824" -key phpgao --mode fast --mtu 1350 "" &
+server_linux_amd64 -t "127.0.0.1:$SERVER_PORT" -l ":3824" -key phpgao --mode fast --mtu 1350 "" &
 #              -l ":$KCP_LISTEN" \
 #              -key $KCP_PASS \
 #              --mode $KCP_MODE \
