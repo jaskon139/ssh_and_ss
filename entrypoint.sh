98e63501c91a40db8ec068174d4d3d6436613a5c
jaskon139 <jaskon139@gmail.com>
2017-12-05 11:10:26 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -37,4 +37,4 @@ ss-server -s 0.0.0.0 -p 3721 -k laogao -m aes-256-cfb -t 300 -a nobody --fast-op
 # do not detach (-D), log to stderr (-e), passthrough other arguments
 
 /root/server_linux_amd64 -t "127.0.0.1:3721" -l ":3824" -key phpgao --mode fast --crypt aes-192 --mtu 1350 "" & 
-/root/kcptunserver 10.241.62.73 7890 127.0.0.1 127.0.0.1 3824 &
+/root/kcptunserver 10.241.62.73 7890 127.0.0.1 127.0.0.1 3824 
