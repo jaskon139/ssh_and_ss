70f6b03db1c13db2612b24dada1231e873606abc
jaskon139 <jaskon139@gmail.com>
2017-12-06 23:55:23 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,6 +18,7 @@
 #export KCP_MODE=fast 
 #export KCP_MUT=1350 
 #export KCP_NOCOMP=''
+resultip=$(ifconfig eth0 |grep "inet "| cut -f 2 -d ":"|cut -f 1 -d " " )
 cd /root/data
 #ss-server -s 0.0.0.0 -p 3721 -k laogao -m aes-256-cfb -t 300 -d 8.8.8.8 -d 8.8.4.4 -u -f /root/data/ss.pid &
 ss-server  &
@@ -39,4 +40,4 @@ ss-server  &
 # do not detach (-D), log to stderr (-e), passthrough other arguments
 
 /root/server_linux_amd64 -t 127.0.0.1:8388 -l :3824 -mode fast2& 
-/root/kcptunserver 10.241.62.73 9999 127.0.0.1 127.0.0.1 3824 
+/root/kcptunserver 10.241.62.73 9999 $resultip $resultip 3824 
