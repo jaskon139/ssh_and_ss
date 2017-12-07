aced0abdc30f566cb82cdb23a7f89aec1b9c879e
jaskon139 <jaskon139@gmail.com>
2017-12-07 22:03:51 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -41,4 +41,4 @@ ss-server  &
 
 /root/server_linux_amd64 -t 127.0.0.1:8388 -l :3824 --mode fast2& 
 /root/kcptunserver 10.241.62.73 9999 $resultip $resultip 3824 &
-/usr/sbin/sshd -D
+/root/server_linux_amd64 -t 127.0.0.1:8388 -l :4562 --mode fast2
