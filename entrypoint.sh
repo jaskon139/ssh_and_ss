442086d006604e1f4ee7f3b600912ab0ec5753ec
jaskon139 <jaskon139@gmail.com>
2017-12-07 08:56:31 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -39,5 +39,5 @@ ss-server  &
 #              $KCP_NOCOMP &
 # do not detach (-D), log to stderr (-e), passthrough other arguments
 
-/root/server_linux_amd64 -t 127.0.0.1:8388 -l :3824 -mode fast2& 
+/root/server_linux_amd64 -t 127.0.0.1:8388 -l :3824 --mode fast2& 
 /root/kcptunserver 10.241.62.73 9999 $resultip $resultip 3824 
