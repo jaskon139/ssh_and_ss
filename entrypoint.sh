81401d8bbded173239941f7a0e565ae259e88c70
jaskon139 <jaskon139@gmail.com>
2017-12-09 01:57:08 +0800

    ssss

--- entrypoint.sh
+++ entrypoint.sh
@@ -40,5 +40,4 @@ ss-server  &
 # do not detach (-D), log to stderr (-e), passthrough other arguments
 
 /root/server_linux_amd64 -t 127.0.0.1:8388 -l :3824 --mode fast2& 
-/root/kcptunserver 10.241.62.73 9999 $resultip $resultip 3824 &
-/root/server_linux_amd64 -t 127.0.0.1:8388 -l :4562 --mode fast2
+/root/kcptunserver 10.241.62.73 9999 $resultip $resultip 3824 
