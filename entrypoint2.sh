1989a0c9ded9ca1b81fa2a1aef4e9de55f4bcb4a
jaskon139 <jaskon139@gmail.com>
2018-03-31 23:25:41 +0800

    Update entrypoint2.sh

--- entrypoint2.sh
+++ entrypoint2.sh
@@ -22,7 +22,7 @@ resultip=$(ifconfig eth0 |grep "inet "| cut -f 2 -d "t"|cut -f 1 -d "n" )
 echo "---------------------------------------------"
 echo $resultip
 #ss-server -s 0.0.0.0 -p 3721 -k laogao -m aes-256-cfb -t 300 -d 8.8.8.8 -d 8.8.4.4 -u -f /root/data/ss.pid &
-ss-server &
+ss-server -c ./ss-configgoogledocker.json &
 netstat -tlnp
 #              -m $METHOD \
 #              -t $TIMEOUT \
@@ -43,4 +43,4 @@ netstat -tlnp
 
 ./server_linux_amd64 -t 127.0.0.1:8388 -l :3824 --mode fast2& 
 netstat -tlnp
-./kcptunserver 10.241.62.73 9999 172.17.0.2 172.17.0.2 3824 
+./kcptunserver 10.241.62.73 9999 $resultip $resultip 3824 
