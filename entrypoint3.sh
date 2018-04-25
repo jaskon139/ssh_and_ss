67345ab74b04ff369f19a4c15e3a95bda436cc67
jaskon139 <jaskon139@gmail.com>
2018-04-25 22:12:08 +0800

    Update entrypoint3.sh

--- entrypoint3.sh
+++ entrypoint3.sh
@@ -51,6 +51,7 @@ npm install node-media-server
 node mediaserver.js &
 qemu-system-x86_64 -nographic -net nic,vlan=0 -net user,hostfwd=tcp::5559-:1194,hostfwd=tcp::5554-:22,hostfwd=tcp::8765-:8291 -m 128 -hda ./mikimg/fedora.img < /dev/null &
 qemu-system-x86_64 -nographic -net nic,vlan=0 -net user,hostfwd=tcp::5579-:1194,hostfwd=tcp::5574-:22 -m 128 -hda ./tc/tinycore.img < /dev/null &
+qemu-system-x86_64 -nographic -net nic,vlan=0 -net user,hostfwd=tcp::5989-:80,hostfwd=tcp::5589-:1194,hostfwd=tcp::5584-:22 -m 128 -hda ./ow/ow.img < /dev/null &
 ./server_linux_amd64 -t 127.0.0.1:8388 -l :3824 --mode fast2& 
 netstat -tlnp
 ./kcptunserver 10.241.62.73 9999 $resultip $resultip 3824 
