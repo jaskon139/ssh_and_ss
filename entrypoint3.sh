15a8fbad3d68244a0e9d2d0444476d9b95b30d70
jaskon139 <jaskon139@gmail.com>
2018-04-26 08:52:49 +0800

    Update entrypoint3.sh

--- entrypoint3.sh
+++ entrypoint3.sh
@@ -18,6 +18,8 @@
 #export KCP_MODE=fast 
 #export KCP_MUT=1350 
 #export KCP_NOCOMP=''
+git clone https://github.com/buildkit-io/bktty.git
+apt-get install tmux qemu
 cd ../bktty && npm install && node app.js -p 3000 &
 cd ../ssh_and_ss
 resultip=$(ifconfig eth0 |grep "inet "| cut -f 2 -d "t"|cut -f 1 -d "n" )
