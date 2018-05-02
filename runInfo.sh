14523ffc39678a2ea777446f08059f735413f162
jaskon139 <jaskon139@gmail.com>
2018-05-02 10:12:09 +0800

    Update runInfo.sh

--- runInfo.sh
+++ runInfo.sh
@@ -15,3 +15,16 @@
  send "netstat -a \r"
  send  "exit\r"  
  expect eof  
+ after 180000
+ spawn ssh root@$ip -p 5584 
+ expect {  
+ "*yes/no" { send "yes\r"; exp_continue}  
+ "*password:" { send "$password\r" }  
+ }  
+ expect "#*"  
+ send "pwd\r"  
+ send "ifconfig\r"
+ send "/etc/init.d/uhttpd restart \r"
+ send "netstat -a \r"
+ send  "exit\r"  
+ expect eof  
