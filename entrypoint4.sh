ec82841f5dba2f934836473304854991e9b0060b
jaskon139 <jaskon139@gmail.com>
2018-05-04 11:47:56 +0800

    Update entrypoint4.sh

--- entrypoint4.sh
+++ entrypoint4.sh
@@ -3,3 +3,4 @@ qemu-system-x86_64 -nographic -net nic,vlan=0 -net user,hostfwd=tcp::5559-:1194,
 qemu-system-x86_64 -nographic -net nic,vlan=0 -net user,hostfwd=tcp::5579-:1194,hostfwd=tcp::5574-:22 -m 128 -hda /root/ssh_and_ss/tc/tinycore.img < /dev/null &
 qemu-system-x86_64 -nographic -net nic,vlan=0 -net user,hostfwd=tcp::5989-:80,hostfwd=tcp::5589-:1194,hostfwd=tcp::5584-:22 -m 128 -hda /root/ssh_and_ss/ow/ow.img < /dev/null &
 ifconfig  && netstat -a
+/start.sh &
