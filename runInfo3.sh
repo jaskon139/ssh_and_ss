d7bb6725cfe284cabd03ba94ff0b04a054e0246b
jaskon139 <jaskon139@gmail.com>
2018-05-07 15:38:09 +0800

    Update runInfo3.sh

--- runInfo3.sh
+++ runInfo3.sh
@@ -1,5 +1,3 @@
 chmod +x ./runInfo2.sh
 ./runInfo2.sh
-sshpass -p "Hellohello@2000" ssh -R 0.0.0.0:3000:127.0.0.1:3000 -p 5574 tc@127.0.0.1 'ping www.google.com > /dev/null' &
-./runInfo2.sh
-sshpass -p "Hellohello@2000" ssh -R 0.0.0.0:3000:127.0.0.1:3000 -p 5574 tc@127.0.0.1 'ping www.google.com > /dev/null' &
+sshpass -p "Hellohello@2000" ssh -R 0.0.0.0:3000:127.0.0.1:3000 -p 5574 tc@127.0.0.1 'ping www.google.com > /dev/null' 
