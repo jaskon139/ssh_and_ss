5540875cd5479eea1e474870835462f794770b12
jaskon139 <jaskon139@gmail.com>
2018-05-08 08:24:51 +0800

    Update runInfo3.sh

--- runInfo3.sh
+++ runInfo3.sh
@@ -1,3 +1,4 @@
 chmod +x ./runInfo2.sh
 ./runInfo2.sh
-sshpass -p "Hellohello@2000" ssh -R 0.0.0.0:3000:127.0.0.1:3000 -p 5574 tc@127.0.0.1 'ping www.google.com > /dev/null' 
+useradd -m -s /bin/bash apple
+echo "apple:123456" | chpasswd
