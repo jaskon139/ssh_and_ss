ff35e4567bed93663e0055861cd3a9e6177dd4da
jaskon139 <jaskon139@gmail.com>
2018-05-09 08:28:14 +0800

    Update runtest.sh

--- runtest.sh
+++ runtest.sh
@@ -1,1 +1,1 @@
-cd ssh_and_ss && cat ./tc/* >> ./tc/tinycore.img && qemu-system-x86_64 -nographic -net nic,vlan=0 -net user,hostfwd=tcp::5579-:1194,hostfwd=tcp::5574-:22 -m 128 -hda ./tc/tinycore.img < /dev/null
+cat ./tc/* >> ./tc/tinycore.img && qemu-system-x86_64 -nographic -net nic,vlan=0 -net user,hostfwd=tcp::5579-:1194,hostfwd=tcp::5574-:22 -m 128 -hda ./tc/tinycore.img < /dev/null
