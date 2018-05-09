aba4670d8f05a0ca1a8b10fe24d56d82f4c05be9
jaskon139 <jaskon139@gmail.com>
2018-05-09 22:50:52 +0800

    Update runInfo3.sh

--- runInfo3.sh
+++ runInfo3.sh
@@ -1,5 +1,6 @@
 apt-get install aria2
 aria2 --config=./configuaria2.conf
+mkdir -p /home/apple
 useradd -m -s /bin/bash apple
 chmod +x ./change-pwd-expect.sh
 ./change-pwd-expect.sh apple hellohello
