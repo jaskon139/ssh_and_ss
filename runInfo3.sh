efe4e25d8d31c0cb7809e747ee7c4f4f7908f4fc
jaskon139 <jaskon139@gmail.com>
2018-05-09 23:02:01 +0800

    Update runInfo3.sh

--- runInfo3.sh
+++ runInfo3.sh
@@ -1,4 +1,8 @@
 apt-get install aria2
+mkdir /etc/aria2    #新建文件夹  
+touch /etc/aria2/aria2.session    #新建session文件
+chmod 777 /etc/aria2/aria2.session    #设置aria2.session可写 
+cp ./configuaria2.conf /etc/aria2/aria2.conf    #创建配置文件
 aria2c --conf-path=./configuaria2.conf -D
 mkdir -p /home/apple
 useradd -m -s /bin/bash apple
