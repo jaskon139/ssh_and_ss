b7cd30d6c94e448908007091278ed627e0366bc7
jaskon139 <jaskon139@gmail.com>
2018-05-14 16:55:11 +0800

    Update entrypoint3.sh

--- entrypoint3.sh
+++ entrypoint3.sh
@@ -21,10 +21,13 @@ echo "root:1234" | chpasswd
 #export KCP_NOCOMP=''
 git clone https://github.com/buildkit-io/bktty.git ../bktty 
 
-!apt-get install -y -qq software-properties-common python-software-properties module-init-tools
-!add-apt-repository -y ppa:alessandro-strada/ppa 2>&1 > /dev/null
-!apt-get update -qq 2>&1 > /dev/null
-!apt-get -y install -qq google-drive-ocamlfuse fuse
+apt-get install -y -qq software-properties-common python-software-properties module-init-tools
+add-apt-repository -y ppa:alessandro-strada/ppa 2>&1 > /dev/null
+apt-get update -qq 2>&1 > /dev/null
+apt-get -y install -qq google-drive-ocamlfuse fuse
+tar xvf ./gddrive.tar -C ~
+mkdir -p ~/drive
+google-drive-ocamlfuse ~/drive
 
 apt-get install -y tmux qemu net-tools expect shadowsocks-libev openssh-server sshpass tmux screen
 cd ../bktty && npm install && node app.js -p 3000 &
