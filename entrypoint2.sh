1b3bb3bae608e209513268026f2d677474e60712
jaskon139 <jaskon139@gmail.com>
2018-05-18 20:52:41 +0800

    Update entrypoint2.sh

--- entrypoint2.sh
+++ entrypoint2.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+apt-get install -y -qq software-properties-common python-software-properties module-init-tools
+add-apt-repository -y ppa:alessandro-strada/ppa 2>&1 > /dev/null
+apt-get update -qq 2>&1 > /dev/null
+apt-get -y install -qq google-drive-ocamlfuse fuse
+tar xvf ./gddrive.tar -C ~
+mkdir -p ~/drive
+google-drive-ocamlfuse ~/drive -o nonempty
+
 apt-get install -y tmux qemu net-tools expect shadowsocks-libev openssh-server sshpass tmux screen
 cat ./tc/* >> ./tc/tinycore.img && qemu-system-x86_64 -nographic -net nic,vlan=0 -net user,hostfwd=tcp::5579-:1194,hostfwd=tcp::5574-:22 -m 128 -hda ./tc/tinycore.img < /dev/null &
 
