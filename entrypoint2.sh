ee628e43015afef132917a31407b2fea3878a84f
jaskon139 <jaskon139@gmail.com>
2018-05-18 23:03:27 +0800

    Update entrypoint2.sh

--- entrypoint2.sh
+++ entrypoint2.sh
@@ -8,7 +8,7 @@ tar xvf ./gddrive.tar -C ~
 mkdir -p ~/drive
 google-drive-ocamlfuse ~/drive -o nonempty
 
-apt-get install -y tmux qemu net-tools expect shadowsocks-libev openssh-server sshpass tmux screen
+apt-get install -y shadowsocks-libev net-tools grep tmux qemu net-tools expect shadowsocks-libev openssh-server sshpass tmux screen 
 cat ./tc/* >> ./tc/tinycore.img && qemu-system-x86_64 -nographic -net nic,vlan=0 -net user,hostfwd=tcp::5579-:1194,hostfwd=tcp::5574-:22 -m 128 -hda ./tc/tinycore.img < /dev/null &
 
 # generate host keys if not present
