78a774123f2b9375391cf6274be60d46a204336c
jaskon139 <jaskon139@gmail.com>
2018-06-02 14:36:04 +0800

    Update entrypointqemu.sh

--- entrypointqemu.sh
+++ entrypointqemu.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-tar xvf /ssh_and_ss/gddrive.tar -C ~
-mkdir -p ~/drive
-google-drive-ocamlfuse ~/drive -o nonempty
-
 qemu-system-x86_64 -nographic -net nic,vlan=0 -net user -m 128 -hda /ssh_and_ss/tc/tinycore.img < /dev/null &
 
 cd /bktty && npm install && node app.js -p 3000
