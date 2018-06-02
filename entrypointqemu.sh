02de1a73686d05642f034a6c49fc2f3fb21ea301
jaskon139 <jaskon139@gmail.com>
2018-06-02 13:20:34 +0800

    Update entrypointqemu.sh

--- entrypointqemu.sh
+++ entrypointqemu.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
 
-tar xvf ./gddrive.tar -C ~
+tar xvf /ssh_and_ss/gddrive.tar -C ~
 mkdir -p ~/drive
 google-drive-ocamlfuse ~/drive -o nonempty
 
-qemu-system-x86_64 -nographic -net nic,vlan=0 -net user,hostfwd=tcp::5579-:1194,hostfwd=tcp::5574-:22 -m 128 -hda ./tc/tinycore.img < /dev/null &
+qemu-system-x86_64 -nographic -net nic,vlan=0 -net user,hostfwd=tcp::5579-:1194,hostfwd=tcp::5574-:22 -m 128 -hda /ssh_and_ss/tc/tinycore.img < /dev/null &
 
-mkdir -p /content/delugeconf
-cp -fr ~/drive/forvmimage/delugeconf/* /content/delugeconf
-deluged -c /content/delugeconf/ &
-deluge-web -c /content/delugeconf/  --base /deluge/ &
+cd /bktty && npm install && node app.js -p 3000 &
