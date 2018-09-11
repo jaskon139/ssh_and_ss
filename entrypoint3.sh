ffd7757dc68d08406f8da5325b1bceda6c59ec04
jaskon139 <jaskon139@gmail.com>
2018-09-11 14:06:30 +0000

    834sdfs4

--- entrypoint3.sh
+++ entrypoint3.sh
@@ -37,6 +37,7 @@ resultip=$(ifconfig eth0 |grep "inet "| cut -f 2 -d "t"|cut -f 1 -d "n" )
 pkill ss-server
 pkill server_linux_amd64
 pkill kcptunserver
+pkill shadowsocks-server-linux64-1.1.5
 echo "---------------------------------------------"
 echo $resultip
 #ss-server -s 0.0.0.0 -p 3721 -k laogao -m aes-256-cfb -t 300 -d 8.8.8.8 -d 8.8.4.4 -u -f /root/data/ss.pid &
