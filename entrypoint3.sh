470c4887c896371a978d78cc18fad4000e28a426
jaskon139 <jaskon139@gmail.com>
2021-11-14 15:32:11 +0800

    23232

--- entrypoint3.sh
+++ entrypoint3.sh
@@ -19,8 +19,8 @@ echo "root:1234" | chpasswd
 #export KCP_MODE=fast 
 #export KCP_MUT=1350 
 #export KCP_NOCOMP=''
-apt-get update
-git clone https://github.com/buildkit-io/bktty.git ../bktty 
+#apt-get update
+#git clone https://github.com/buildkit-io/bktty.git ../bktty 
 
 #apt-get install -y -qq software-properties-common python-software-properties module-init-tools
 #add-apt-repository -y ppa:alessandro-strada/ppa 2>&1 > /dev/null
@@ -31,7 +31,7 @@ git clone https://github.com/buildkit-io/bktty.git ../bktty
 #google-drive-ocamlfuse ~/drive -o nonempty
 
 apt-get install -y tmux qemu net-tools expect shadowsocks-libev openssh-server sshpass tmux screen vim
-cd ../bktty && npm install && node app.js -p 3000 &
+#cd ../bktty && npm install && node app.js -p 3000 &
 cd ../ssh_and_ss && rm identity.secret && mv identity3.secret identity.secret
 resultip=$(ifconfig eth0 |grep "inet "| cut -f 2 -d "t"|cut -f 1 -d "n" )
 pkill ss-server
@@ -46,25 +46,7 @@ echo $resultip
 chmod +x ./*.sh
 chmod +x ./shadowsocks-server-linux64-1.1.5 && ./shadowsocks-server-linux64-1.1.5 -c ./ss-configcodeing.json &
 netstat -tlnp
-#              -m $METHOD \
-#              -t $TIMEOUT \
-#              -a $USER \
-#              $FASTOPEN \
-#              -d $DNS_ADDR \
-#              -d $DNS_ADDR_2 \
-#              $UDP \
-#              -f /tmp/ss.pid &
-#server_linux_amd64 -t "127.0.0.1:3721" -l ":3824" -key phpgao --mode fast --crypt aes-192 --mtu 1350 "" &
-#              -l ":$KCP_LISTEN" \
-#              -key $KCP_PASS \
-#              --mode $KCP_MODE \
-#              --crypt $KCP_ENCRYPT \
-#              --mtu $KCP_MUT \
-#              $KCP_NOCOMP &
-# do not detach (-D), log to stderr (-e), passthrough other arguments
 mkdir -p /run/sshd && /usr/sbin/sshd &
-#npm install node-media-server
-#node mediaserver.js &
 #cat ./mikimg/* >> ./mikimg/fedora.img && qemu-system-x86_64 -nographic -net nic,vlan=0 -net user,hostfwd=tcp::5559-:1194,hostfwd=tcp::5554-:22,hostfwd=tcp::8765-:8291 -m 128 -hda ./mikimg/fedora.img < /dev/null &
 #cat ./tc/* >> ./tc/tinycore.img && qemu-system-x86_64 -nographic -net nic,vlan=0 -net user,hostfwd=tcp::5579-:1194,hostfwd=tcp::5574-:22 -m 512 -hda ./tc/tinycore.img < /dev/null &
 #cat ./ow/* >> ./ow/ow.img && qemu-system-x86_64 -nographic -net nic,vlan=0 -net user,hostfwd=tcp::5989-:80,hostfwd=tcp::5589-:1194,hostfwd=tcp::5584-:22 -m 128 -hda ./ow/ow.img < /dev/null &
