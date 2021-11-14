93cc80e38da47cdd213f6d47d464d60b548ddefd
jaskon139 <jaskon139@gmail.com>
2021-11-14 16:24:30 +0800

    2323

--- entrypoint3.sh
+++ entrypoint3.sh
@@ -2,12 +2,12 @@
 echo "root:1234" | chpasswd 
 
 #apt-get install -y -qq software-properties-common python-software-properties module-init-tools
-#add-apt-repository -y ppa:alessandro-strada/ppa 2>&1 > /dev/null
+add-apt-repository -y ppa:alessandro-strada/ppa 2>&1 > /dev/null
 #apt-get update -qq 2>&1 > /dev/null
-#apt-get -y install -qq google-drive-ocamlfuse fuse
-#tar xvf ./gddrive.tar -C ~
-#mkdir -p ~/drive
-#google-drive-ocamlfuse ~/drive -o nonempty
+apt-get -y install -qq google-drive-ocamlfuse fuse
+tar xvf ./gddrive.tar -C ~
+mkdir -p ~/drive
+google-drive-ocamlfuse ~/drive -label me  -o nonempty
 
 apt-get install -y tmux qemu net-tools expect openssh-server sshpass tmux screen vim
 cd ../ssh_and_ss && rm identity.secret && mv identity3.secret identity.secret
