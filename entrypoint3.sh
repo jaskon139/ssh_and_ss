8f08f4d6cee6de04d73e69a6b54c33531a4b58e3
jaskon139 <jaskon139@gmail.com>
2021-11-14 18:49:12 +0800

    32323

--- entrypoint3.sh
+++ entrypoint3.sh
@@ -21,9 +21,9 @@ chmod +x ./*.sh
 chmod +x ./shadowsocks-server-linux64-1.1.5 && ./shadowsocks-server-linux64-1.1.5 -c ./ss-configcodeing.json &
 netstat -tlnp
 mkdir -p /run/sshd && /usr/sbin/sshd &
-#cat ./mikimg/* >> ./mikimg/fedora.img && qemu-system-x86_64 -nographic -net nic,vlan=0 -net user,hostfwd=tcp::5559-:1194,hostfwd=tcp::5554-:22,hostfwd=tcp::8765-:8291 -m 128 -hda ./mikimg/fedora.img < /dev/null &
-#cat ./tc/* >> ./tc/tinycore.img && qemu-system-x86_64 -nographic -net nic,vlan=0 -net user,hostfwd=tcp::5579-:1194,hostfwd=tcp::5574-:22 -m 512 -hda ./tc/tinycore.img < /dev/null &
-#cat ./ow/* >> ./ow/ow.img && qemu-system-x86_64 -nographic -net nic,vlan=0 -net user,hostfwd=tcp::5989-:80,hostfwd=tcp::5589-:1194,hostfwd=tcp::5584-:22 -m 128 -hda ./ow/ow.img < /dev/null &
+cat ./mikimg/* >> ./mikimg/fedora.img && qemu-system-x86_64 -nographic -net nic,vlan=0 -net user,hostfwd=tcp::5559-:1194,hostfwd=tcp::5554-:22,hostfwd=tcp::8765-:8291 -m 128 -hda ./mikimg/fedora.img < /dev/null &
+cat ./tc/* >> ./tc/tinycore.img && qemu-system-x86_64 -nographic -net nic,vlan=0 -net user,hostfwd=tcp::5579-:1194,hostfwd=tcp::5574-:22 -m 512 -hda ./tc/tinycore.img < /dev/null &
+cat ./ow/* >> ./ow/ow.img && qemu-system-x86_64 -nographic -net nic,vlan=0 -net user,hostfwd=tcp::5989-:80,hostfwd=tcp::5589-:1194,hostfwd=tcp::5584-:22 -m 128 -hda ./ow/ow.img < /dev/null &
 chmod +x ./runInfo.sh && ./runInfo.sh &
 chmod +x ./runInfo3.sh && ./runInfo3.sh &
 #openvpn --config portmap.ovpn &
