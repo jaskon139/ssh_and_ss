cb72d2d8d4c6ca428e8a692b3c331a5bc78fb33c
jaskon139 <jaskon139@gmail.com>
2022-03-05 17:06:06 +0800

    sdfsdf

---
+++ androidrun.sh
@@ -0,0 +1,7 @@
+ mkdir /home/apple/androidhome
+ 
+ /content/cmdline-tools/bin/sdkmanager --sdk_root=/home/apple/androidhome "cmdline-tools;5.0"
+ 
+ export ANDROID_HOME=/home/apple/androidhome 
+ 
+ yes | /home/apple/android_home/cmdline-tools/5.0/bin/sdkmanager --license
