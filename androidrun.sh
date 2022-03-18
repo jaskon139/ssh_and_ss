9e5eabf3f0b4ee16a920a011513994407bc9dd30
jaskon139 <jaskon139@gmail.com>
2022-03-18 20:03:24 +0800

    2323

--- androidrun.sh
+++ androidrun.sh
@@ -29,7 +29,7 @@ export GOROOT=/home/apple/go
 
 export PATH=$GOROOT/bin:$PATH
 
-sudo  apt -y install xfce4 xfce4-goodies xterm
+sudo  apt -y install xfce4 xfce4-goodies xterm vnc4server
 
 mkdir ~/.vnc; cp /content/ssh_and_ss/vncserver.config ~/.vnc/xstartup; chmod +x ~/.vnc/xstartup
 
