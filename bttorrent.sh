dd11b5304519cb0df2c59b08a30185b3d12a3154
jaskon139 <jaskon139@gmail.com>
2022-05-04 17:36:51 +0800

    2323

--- bttorrent.sh
+++ bttorrent.sh
@@ -1,3 +1,6 @@
 wget -q --no-check-certificate raw.githubusercontent.com/sayem314/My-Deluge-Installer/master/install-deluge.sh -O install-deluge.sh ; chmod +x install-deluge.sh
 
 sudo ./install-deluge.sh -install
+
+
+deluge-web &
